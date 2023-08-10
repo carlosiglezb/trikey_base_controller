aa747cd65a03b4cffef5619689377c2412b8545c
Aarim02 <akim2002@utexas.com>
2023-08-10 13:45:34 -0500

    Add 120 matrix rotation

--- src/omniwheel_kinematics.cpp
+++ src/omniwheel_kinematics.cpp
@@ -13,16 +13,17 @@ OmniwheelKinematics::OmniwheelKinematics()
 
 void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chassis_center)
 {
-    H_mat(0, 0) = wheel_to_chassis_center;
-    H_mat(1, 0) = wheel_to_chassis_center;
+    H_mat(0, 0) = -wheel_to_chassis_center/2 - wheel_to_chassis_center*sqrt(3)/2;
+    H_mat(1, 0) = sqrt(3)*wheel_to_chassis_center/2 - wheel_to_chassis_center/2;
     H_mat(2, 0) = wheel_to_chassis_center;
 
-    H_mat(2, 1) = 1.0;
-    H_mat(0, 1) = -0.5;
-    H_mat(1, 1) = -0.5;
+    H_mat(0, 1) = sqrt(3)/4 - 1/2;
+    H_mat(1, 1) = sqt(3)/2 +1/4;
+    H_mat(2, 1) = -0.5;
 
-    H_mat(0,2) = -sin(4.*M_PI/3.0);
-    H_mat(1,2) = sin(4.*M_PI/3.0);
+    H_mat(0,2) = -3/4;
+    H_mat(1,2) = -sqrt(3)/4;
+    H_mat(2,2) = -sqrt(3)/2;
 
     H_mat /= -wheel_radius;
 }
