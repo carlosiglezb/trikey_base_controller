96719746fe6c33000945103756e1fa9a5b9c5f5c
Steven Ortega <stevencortega99@gmail.com>
2023-08-10 18:47:47 -0500

    Fixed xyz coord on controller. Lidar on Y-axis. Does not fix RVIZ

--- src/omniwheel_kinematics.cpp
+++ src/omniwheel_kinematics.cpp
@@ -9,36 +9,24 @@ OmniwheelKinematics::OmniwheelKinematics()
     wheel_vel_limit_ = 0.0;
 
     H_mat.setZero();
-
-    R_mat.setZero();
-
 }
 
 void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chassis_center)
 {
-
-
     H_mat(0, 0) = wheel_to_chassis_center;
     H_mat(1, 0) = wheel_to_chassis_center;
     H_mat(2, 0) = wheel_to_chassis_center;
 
-    H_mat(0, 1) = 1.0;
-    H_mat(1, 1) = -0.5;
-    H_mat(2, 1) = -0.5;
+    H_mat(0, 1) = 0.5;
+    H_mat(1, 1) = 0.5;
+    H_mat(2, 1) = -1.0;
 
+    H_mat(0,2) = sin(4.*M_PI/3.0);
     H_mat(1,2) = -sin(4.*M_PI/3.0);
-    H_mat(2,2) = sin(4.*M_PI/3.0);
-    
+    H_mat(2,2) = 0;
+
 
     H_mat /= -wheel_radius;
-
-    R_mat(0,0) = cos(M_PI/3.);
-    R_mat(1,0) = sin(M_PI/3.);
-    R_mat(0,1) = -sin(M_PI/3.);
-    R_mat(1,1) = cos(M_PI/3.);
-    R_mat(3,3) = 1.;
-
-    H_mat *= R_mat;
 }
 
 Eigen::Matrix3d OmniwheelKinematics::get_H() {return H_mat;}
