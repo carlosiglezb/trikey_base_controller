725d4c6a1cc0abcbcf0485c508f63eb164e72126
Aarim02 <akim2002@utexas.com>
2023-08-10 15:47:03 -0500

    Fixing H-Matrix #3

--- src/omniwheel_kinematics.cpp
+++ src/omniwheel_kinematics.cpp
@@ -9,22 +9,15 @@ OmniwheelKinematics::OmniwheelKinematics()
     wheel_vel_limit_ = 0.0;
 
     H_mat.setZero();
+
+    R_mat.setZero();
+
 }
 
 void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chassis_center)
 {
-    // H_mat(0, 0) = wheel_to_chassis_center*cos(2.094)+sin(2.094);
-    // H_mat(1, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
-    // H_mat(2, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
 
-    // H_mat(0, 1) = -wheel_to_chassis_center*sin(2.094)+cos(2.094);
-    // H_mat(1, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
-    // H_mat(2, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
 
-    // H_mat(0,2) = 0;
-    // H_mat(1,2) = -sqrt(3)/2;
-    // H_mat(2,2) = -sqrt(3)/2;
-    
     H_mat(0, 0) = wheel_to_chassis_center;
     H_mat(1, 0) = wheel_to_chassis_center;
     H_mat(2, 0) = wheel_to_chassis_center;
@@ -39,6 +32,13 @@ void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chas
 
     H_mat /= -wheel_radius;
 
+    R_mat(0,0) = cos(M_PI/3.);
+    R_mat(1,0) = sin(M_PI/3.);
+    R_mat(0,1) = -sin(M_PI/3.);
+    R_mat(1,1) = cos(M_PI/3.);
+    R_mat(3,3) = 1.;
+
+    H_mat *= R_mat;
 }
 
 Eigen::Matrix3d OmniwheelKinematics::get_H() {return H_mat;}
