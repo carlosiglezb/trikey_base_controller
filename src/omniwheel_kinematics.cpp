d49b511d33b882f79b52e83cc356a947bb947241
Aarim02 <akim2002@utexas.com>
2023-08-10 14:20:14 -0500

    fixed #2: h matrix

--- src/omniwheel_kinematics.cpp
+++ src/omniwheel_kinematics.cpp
@@ -13,19 +13,20 @@ OmniwheelKinematics::OmniwheelKinematics()
 
 void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chassis_center)
 {
-    H_mat(0, 0) = -wheel_to_chassis_center/2 - wheel_to_chassis_center*sqrt(3)/2;
-    H_mat(1, 0) = sqrt(3)*wheel_to_chassis_center/2 - wheel_to_chassis_center/2;
-    H_mat(2, 0) = wheel_to_chassis_center;
+    H_mat(0, 0) = wheel_to_chassis_center*cos(2.094)+sin(2.094);
+    H_mat(1, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
+    H_mat(2, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
 
-    H_mat(0, 1) = sqrt(3)/4 - 1/2;
-    H_mat(1, 1) = sqt(3)/2 +1/4;
-    H_mat(2, 1) = -0.5;
+    H_mat(0, 1) = -wheel_to_chassis_center*sin(2.094)+cos(2.094);
+    H_mat(1, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
+    H_mat(2, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
 
-    H_mat(0,2) = -3/4;
-    H_mat(1,2) = -sqrt(3)/4;
+    H_mat(0,2) = 0;
+    H_mat(1,2) = -sqrt(3)/2;
     H_mat(2,2) = -sqrt(3)/2;
 
     H_mat /= -wheel_radius;
+
 }
 
 Eigen::Matrix3d OmniwheelKinematics::get_H() {return H_mat;}
