4c0190c3eea63cbd4e126a043ab5da68f77572a2
Aarim02 <akim2002@utexas.com>
2023-08-10 14:52:30 -0500

    revert changes h matrix

--- src/omniwheel_kinematics.cpp
+++ src/omniwheel_kinematics.cpp
@@ -13,17 +13,29 @@ OmniwheelKinematics::OmniwheelKinematics()
 
 void OmniwheelKinematics::initialize_H(double wheel_radius, double wheel_to_chassis_center)
 {
-    H_mat(0, 0) = wheel_to_chassis_center*cos(2.094)+sin(2.094);
-    H_mat(1, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
-    H_mat(2, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
+    // H_mat(0, 0) = wheel_to_chassis_center*cos(2.094)+sin(2.094);
+    // H_mat(1, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
+    // H_mat(2, 0) = wheel_to_chassis_center*cos(2.094)-0.43311144;
 
-    H_mat(0, 1) = -wheel_to_chassis_center*sin(2.094)+cos(2.094);
-    H_mat(1, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
-    H_mat(2, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
+    // H_mat(0, 1) = -wheel_to_chassis_center*sin(2.094)+cos(2.094);
+    // H_mat(1, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
+    // H_mat(2, 1) = -wheel_to_chassis_center*sin(2.094)+0.48595369;
 
-    H_mat(0,2) = 0;
-    H_mat(1,2) = -sqrt(3)/2;
-    H_mat(2,2) = -sqrt(3)/2;
+    // H_mat(0,2) = 0;
+    // H_mat(1,2) = -sqrt(3)/2;
+    // H_mat(2,2) = -sqrt(3)/2;
+    
+    H_mat(0, 0) = wheel_to_chassis_center;
+    H_mat(1, 0) = wheel_to_chassis_center;
+    H_mat(2, 0) = wheel_to_chassis_center;
+
+    H_mat(0, 1) = 1.0;
+    H_mat(1, 1) = -0.5;
+    H_mat(2, 1) = -0.5;
+
+    H_mat(1,2) = -sin(4.*M_PI/3.0);
+    H_mat(2,2) = sin(4.*M_PI/3.0);
+    
 
     H_mat /= -wheel_radius;
 
