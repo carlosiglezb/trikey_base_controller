099615f7fc2b7cdf3b407ca2f8e4f3ca2070757d
Steven Ortega <stevencortega99@gmail.com>
2024-10-17 16:06:57 -0500

    modified:   launch/trikey_base_controller.launch 	modified:   src/trikey_base_controller.cpp

--- src/trikey_base_controller.cpp
+++ src/trikey_base_controller.cpp
@@ -313,7 +313,7 @@ namespace trikey_base_controller
 
         //compute odometry and publish odom topic and tf (optional)
         computeOdometry(filtered_velocities_, wheel_odom_, time);
-        updateOdometry(wheel_odom_, publish_odom_wheel_tf_);
+       // updateOdometry(wheel_odom_, publish_odom_wheel_tf_);
 
         // update previous timestamp
         timestamp_prev_ = timestamp_now;
